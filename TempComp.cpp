98b550f9d174bfc175d2ed9b1c602d4aa67a535b
Tyler Orr <Tyler_Orr@baylor.edu>
2012-03-03 16:13:57 -0600

    Added temporary component for testing purposes

---
+++ TempComp.cpp
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ Filename:                  TempComp.cpp
+ Classname:                 TempComp
+ 
+ Description:               This file declares the TempComp class. It is just
+ used for testing purposes to instantiate a subclass
+ of the abstract class, Component.
+ 
+ Last Modified:            				03.02.12
+ By:									Tyler Orr
+ - File created
+ ******************************************************************************/
+
+#include "TempComp.h"
+#include <iostream>
+
+using namespace std;
+
+/*******************************************************************************
+ Name:              TempComp
+ Description:       Default constructor for TempComp class
+ ******************************************************************************/
+TempComp::TempComp() : Component()
+{
+    numData = 0;
+    charData = new char[4];
+    charData = "ABC";
+}
+
+/*******************************************************************************
+ Name:              TempComp
+ Description:       Copy constructor for TempComp class
+ ******************************************************************************/
+TempComp::TempComp(const TempComp& other)
+{
+    numData = other.numData;
+    charData = new char[strlen(other.charData)];
+    strcpy(charData, other.charData);
+}
+
+/*******************************************************************************
+ Name:              ~TempComp
+ Description:       Destructor for TempComp class
+ ******************************************************************************/
+TempComp::~TempComp()
+{
+    delete [] charData;
+}
+
+/*******************************************************************************
+ Name:              load
+ Description:       This method loads the TempComp
+ 
+ Output:
+    returns         bool value of whether the TempComp loaded correctly
+ ******************************************************************************/
+bool TempComp::load()
+{
+    return true;
+}
+
+/*******************************************************************************
+ Name:              update
+ Description:       This method updates the TempComp
+ ******************************************************************************/
+int TempComp::update()
+{
+    return 0;
+}
+
+
---
+++ TempComp.h
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ Filename:                  TempComp.h
+ Classname:                 TempComp
+ 
+ Description:               This file declares the TempComp class. It is just
+                            used for testing purposes to instantiate a subclass
+                            of the abstract class, Component.
+ 
+ Last Modified:            				03.02.12
+ By:									Tyler Orr
+ - File created
+ ******************************************************************************/
+
+#ifndef AngrySomething_TempComp_h
+#define AngrySomething_TempComp_h
+
+#include "Component.h"
+#include <SDL/SDL.h>
+
+class TempComp : public Component
+{
+private:
+    int     numData;
+    char    *charData;
+    
+public:
+    TempComp();
+    TempComp(const TempComp&);
+    ~TempComp();
+    
+    virtual bool    load();
+    virtual int     update();
+};
+
+#endif
