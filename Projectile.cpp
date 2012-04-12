217d7b0e834bca34ad32e72c004649080272fa9d
Tyler Orr <Tyler_Orr@baylor.edu>
2012-04-12 00:58:51 -0500

    merged Tim's changes and additions to pig and wall

--- Projectile.cpp
+++ Projectile.cpp
@@ -47,9 +47,30 @@ void Projectile::run()
 {
     move();
 
-//    if(abs(vel.x) < .5 && abs(vel.y) < .5)
-//    {
-//        state = -1;
-//    }
+    if(abs(vel.x) < .5 && abs(vel.y) < .5)
+    {
+        state = -1;
+    }
+}
+
+void Projectile::applyForce(int m, vect v, int dir)
+{
+    if(dir == 0)
+    {
+        v.y = vel.y * .8;   //friction
+        acc.x += ((m * (v.x - vel.x)) / mass) * .8;
+        acc.y += (v.y - vel.y) * .8;
+    }
+    else if(dir == 1)
+    {
+        v.x = vel.x * .8;   //friction
+        acc.x += (v.x - vel.x) * .8;
+        acc.y += ((m * (v.y - vel.y)) / mass) * .8;
+    }
+    else
+    {
+        acc.x += ((m * (v.x - vel.x)) / mass) * .8;
+        acc.y += ((m * (v.y - vel.y)) / mass) * .8;
+    }
 }
 
