c95743c4d11380430c5bf87deb052d92752edc5f
Tyler Orr <Tyler_Orr@baylor.edu>
2012-04-12 20:16:24 -0500

    merged changes from Tim

--- Projectile.cpp
+++ Projectile.cpp
@@ -7,6 +7,7 @@
 
 #include "Projectile.h"
 #include <cmath>
+
 Projectile::Projectile(const char* file, int x, int y, int vx, int vy)
     :   DrawableObject(file),
         PhysicalObject(vx, vy),
@@ -46,7 +47,7 @@ Projectile& Projectile::operator=(const Projectile& other)
 void Projectile::run()
 {
     move();
-
+    
     if(pow((pow(vel.y,2) + pow(vel.x, 2)), .5) < 1)
     {
         state = -1;
@@ -59,10 +60,14 @@ void Projectile::applyForce(int m, vect v, int dir)
 {
     if(dir == 0)
     {
+        v.y = vel.y * .8;   //friction
         acc.x += ((m * (v.x - vel.x)) / mass) * .8;
+        acc.y += (v.y - vel.y) * .8;
     }
     else if(dir == 1)
     {
+        v.x = vel.x * .8;   //friction
+        acc.x += (v.x - vel.x) * .8;
         acc.y += ((m * (v.y - vel.y)) / mass) * .8;
     }
     else
